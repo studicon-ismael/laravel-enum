e03537c2f90a4ecf8256951665e4c24350b9572f
Vyacheslav Basenko <modern.web.artisan@gmail.com>
2017-06-27 18:49:38 +0300

    Added resources registration and example migration stub

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -12,17 +12,7 @@ class SkeletonServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
-            ], 'config');
-
-            /*
-            $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
-
-            $this->publishes([
-                __DIR__.'/../resources/views' => base_path('resources/views/vendor/skeleton'),
-            ], 'views');
-            */
+            $this->registerResources();
         }
     }
 
@@ -32,5 +22,43 @@ class SkeletonServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'skeleton');
+    }
+
+    /**
+     * Register package resources.
+     */
+    protected function registerResources()
+    {
+        // Config
+        $this->publishes([
+            __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
+        ], 'config');
+
+        // Views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
+
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/skeleton'),
+        ], 'views');
+
+        // Database
+        if (! class_exists('CreateSocialProvidersTable')) {
+            // Publish the migration
+            $timestamp = date('Y_m_d_His', time());
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_skeleton_table.php.stub' =>
+                $this->app->databasePath().'/migrations/'.$timestamp.'_create_skeleton_table.php',
+            ], 'migrations');
+        }
+
+        // Translations
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'skeleton');
+
+        $this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/social-auth'),
+        ], 'lang');
+
+        // Routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
     }
 }
